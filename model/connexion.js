dfb818025d389e1abc611d326f8ee9ab5fff2aca
amineBoukhari <146739783+amineBoukhari@users.noreply.github.com>
2024-09-26 08:48:52 +0200

    connexion creation

---
+++ model/connexion.js
@@ -0,0 +1,13 @@
+const mongoose = require ("mongoose")
+
+async function connect() {
+    try {
+        await mongoose.connect("mongodb://127.0.0.1:27017/réseau")
+        console.log("successfuly connect ")
+    }
+   catch(err){
+    console.log(err)
+   }
+}
+
+module.exports = connect;
