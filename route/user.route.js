d46a6db7db29feb210478bf18fc32757f5c2f482
YacineMdh <yacine.meddahi@hotmail.com>
2024-09-26 10:58:45 +0200

    take with id

--- route/user.route.js
+++ route/user.route.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const userController = require("./../controller/user.controller");
 
-router.post("/login", userController.login);
 
+router.post("/login", userController.login);
 router.post("/signin", userController.signin);
 
 module.exports = router;
