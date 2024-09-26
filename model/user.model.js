003787d8959035cccebe50cef2be62b4621328ff
YacineMdh <yacine.meddahi@hotmail.com>
2024-09-26 08:53:45 +0200

    add user model

--- model/user.model.js
+++ model/user.model.js
@@ -3,3 +3,16 @@
  * 
  * Un user doit avoir au minimum : un login (unique) et un mot de passe
  */
+
+const { Schema, model } = require("mongoose");
+const bcrypt = require('bcrypt');
+
+
+const user = new Schema
+({
+    login : { type : String , required:true, unique:true} ,
+    password :{type : String, required:true}
+
+})
+
+module.exports = model('User',user);
