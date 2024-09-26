58aa7674a0dd6af087bb12725fd097072e588b4a
thibaultcrn1 <tcarron6@outlook.com>
2024-09-26 08:53:09 +0200

    Create comment model

--- model/comment.model.js
+++ model/comment.model.js
@@ -3,3 +3,13 @@
  * 
  * Un post doit avoir au minimum : un message, une date, un userId (auteur du post) et un postId
  */
+
+const { Schema, SchemaTypes, model } = require('mongoose');
+
+const Comment = new Schema({
+    message: { type: SchemaTypes.String, required: true },
+    userId: { type: SchemaTypes.String, required: true },
+    postId: { type: SchemaTypes.String, required: true }
+})
+
+module.exports = model('comment', Comment);
